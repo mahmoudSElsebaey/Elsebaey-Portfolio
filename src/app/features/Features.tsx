0539c369ce37b87bff6fa9cf6621ffd9094fb2a5
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-06-07 06:41:46 +0300

    Enhance FeaturesTabs component styling and interactivity, improve list item animations and visibility

--- src/app/features/Features.tsx
+++ src/app/features/Features.tsx
@@ -74,11 +74,13 @@ export default function FeaturesTabs() {
                   {section.features.map((f, i) => (
                     <li
                       key={i}
-                      className="text-[16px] flex items-center gap-3 text-white/60 p-2 hover:text-white"
+                      className="text-[16px] flex items-center gap-3 text-white/60 p-2 hover:text-white group transition-all duration-300"
                     >
                       <span className="font-bold text-outline text-transparent animate-pulse text-2xl">
-                        {/* 0{i+1} */}
-                        {String(i + 1).padStart(2, "0")}
+                       <span className={`${i <= 9 ? "hidden" : ""}`}>0</span>
+                       <span className=" group-hover:scale-[1.2] transition-all duration-300 ">{i+1}</span>
+                      
+                        {/* {String(i + 1).padStart(2, "0")} */}
                       </span>
                       <span className="">{f}</span>
                     </li>
