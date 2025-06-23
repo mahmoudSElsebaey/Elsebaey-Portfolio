8613e25c5c97bc5ae84e2498b18b892dd3756c1a
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-06-23 22:06:56 +0300

    Adjust typography in Title component for better responsiveness and readability

--- src/components/TitleSections/Title.tsx
+++ src/components/TitleSections/Title.tsx
@@ -6,13 +6,13 @@ type TitleProps = {
 export default function Title({ mainTitle, subTitle }: TitleProps) {
   return (
     <div className="pt-15">
-      <h2 className=" text-center text-6xl text-primary-1000 uppercase font-extrabold">{mainTitle}</h2>
+      <h2 className=" text-center text-4xl md:text-6xl text-primary-1000 uppercase font-extrabold">{mainTitle}</h2>
       <div className="flex justify-center items-center gap-2 ">
         <div className="flex flex-col gap-1 w-[15%] md:w-[25%]">
           <span className="h-[1px] bg-primary-1000/70 "></span>
           <span className="h-[1px] bg-primary-1000/70 "></span>
         </div>
-        <h3 className="w-fit text-center dark:text-white/70 text-black/70 capitalize animate-pulse ">
+        <h3 className="w-fit text-center text-[13px] md:text-[16px] dark:text-white/70 text-black/70 capitalize animate-pulse ">
          {subTitle}
         </h3>
         <div className="flex flex-col gap-1 w-[15%] md:w-[25%]">
