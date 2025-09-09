110caaaf0f97bd9c4644851844ed0fc44fd48019
Mahmoud Elsebaey <mahmoudelsebaey710@gmail.com>
2025-09-09 23:33:57 +0300

    refactor: styling fo Stats component

--- src/components/ui/Stats.tsx
+++ src/components/ui/Stats.tsx
@@ -17,19 +17,28 @@ export default function Stats() {
   return (
     <section className="">
       <div className="container mx-auto">
-      <div className="flex flex-wrap gap-6 max-w-[90vw] xl:max-w-none mx-auto">
-        {stats.map((item, index) => (
-          <div key={index} className="flex flex-1 gap-4 items-center justify-start">
-            <CountUp
-              end={item.num}
-              duration={5}
-              delay={2}
-              className="text-4xl xl:text-6xl font-extrabold text-primary-1000"
-            />
-            <p className={`${item.text.length<15?"max-w-[100px]":"max-w-[150px]"} leading-snug text-gray-500/80`}>{item.text}</p>
-          </div>
-        ))}
-      </div>
+        <div className="flex flex-wrap gap-6 max-w-[90vw] xl:max-w-none mx-auto">
+          {stats.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-1 gap-4 items-center justify-start"
+            >
+              <CountUp
+                end={item.num}
+                duration={5}
+                delay={2}
+                className="text-3xl xl:text-6xl font-extrabold text-primary-1000"
+              />
+              <p
+                className={`${
+                  item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
+                } leading-snug text-gray-500/80 sm:text-xs md:text-base`}
+              >
+                {item.text}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
